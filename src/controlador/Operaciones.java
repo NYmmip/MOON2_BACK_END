6c105ce2f61b32e2ec5a88d00a7515ff796948ec
NYmmip <baldionjd@hotmail.com>
2021-05-23 17:25:37 -0500

    Se completaron las Operaciones CRUD de usuarios y productos.

--- src/controlador/Operaciones.java
+++ src/controlador/Operaciones.java
@@ -2,7 +2,9 @@ package controlador;
 
 import modelo.Producto;
 
+import javax.swing.plaf.nimbus.State;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.SortedMap;
 
@@ -21,11 +23,17 @@ public class Operaciones {
 
     public static void main(String[] args) throws SQLException {
         operaciones = Operaciones.crearIFacade();
-        System.out.println(operaciones.deleteProducto("123123"));
-        String[] temp = operaciones.getProducto("123123");
-        for (String c:temp) {
-            System.out.println(c);
-        }
+        //operaciones.addProducto("32124","nombew","asdsaeews",53322,6734323,"3x53x23");
+
+        //String[] temp = operaciones.getProducto("123123");
+        //for (String[] c:operaciones.getAllProductos()) {
+            //for (String x:c) {
+                //.out.println(x);
+            //}
+        //}
+        //operaciones.createUser("Admin1","user2","Usuario2","312112","pass1",0);
+        //operaciones.setUser("Admin1","user1","Usuario2","312112","pass1",0);
+        System.out.println(operaciones.getAllProductos().size());
     }
 
     public boolean addProducto(String id, String nombre, String descripcion, int cantidad, float precio, String dimensiones) throws SQLException {
@@ -39,7 +47,7 @@ public class Operaciones {
             if (rows > 0)
                 System.out.println("Row Inserted");
             connection.close();
-            System.out.println("Connection Closed");
+            System.out.println("Connection Closed \n");
             return true;
         } catch (Exception e) {
             System.out.println("ERROR Inserting Row");
@@ -51,6 +59,7 @@ public class Operaciones {
     public boolean setProducto(String id, String nombre, String descripcion, int cantidad, float precio, String dimensiones) {
 
         try {
+            boolean temp = false;
             Connection connection = this.createConnection();
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery("SELECT ID FROM Productos WHERE ID = "+id);
@@ -59,12 +68,14 @@ public class Operaciones {
 
             if (resultSet.next() == false) {
                 System.out.println("No results");
+                System.out.println("Connection Closed \n");
                 return false;
             } else {
                 System.out.println("Producto Found");
+                temp = true;
             }
 
-            if(resultSet.next()){
+            if(temp){
                 String query = "DELETE FROM Productos "+
                         "WHERE ID = "+id;
                 System.out.println("Deleting....");
@@ -78,7 +89,7 @@ public class Operaciones {
             }
 
             connection.close();
-            System.out.println("Connection Closed");
+            System.out.println("Connection Closed \n");
             return true;
         } catch (Exception e) {
             System.out.println("Error Updating");
@@ -93,10 +104,11 @@ public class Operaciones {
             String[] temp = new String[6];
             PreparedStatement statement = connection.prepareStatement("SELECT * FROM Productos WHERE ID = ?");
             statement.setString(1,id);
-            ResultSet resultSet = statement.executeQuery();
             System.out.println("Searching Producto....");
+            ResultSet resultSet = statement.executeQuery();
             if (resultSet.next() == false) {
                 System.out.println("No results");
+                System.out.println("Connection Closed \n");
                 connection.close();
                 return null;
             } else {
@@ -109,7 +121,7 @@ public class Operaciones {
                     temp[5] = resultSet.getString("dimensiones");
                 } while (resultSet.next());
                 System.out.println("Producto Found");
-                System.out.println("Connection Closed");
+                System.out.println("Connection Closed \n");
                 connection.close();
                 return temp;
             }
@@ -122,32 +134,210 @@ public class Operaciones {
 
     public boolean deleteProducto(String id) {
         try {
+            boolean temp = false;
             Connection connection = this.createConnection();
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT ID FROM Productos WHERE ID = "+id);
+            ResultSet resultSet = statement.executeQuery("SELECT ID FROM Productos WHERE ID = '"+id+"'");
 
             System.out.println("Searching Producto....");
 
             if (resultSet.next() == false) {
                 System.out.println("No results");
+                System.out.println("Connection Closed \n");
+                connection.close();
                 return false;
             } else {
                 System.out.println("Producto Found");
+                temp = true;
             }
 
-            if(resultSet.next()) {
+            if(temp) {
                 String query = "DELETE FROM Productos " +
                         "WHERE ID = " + id;
                 System.out.println("Deleting....");
                 statement.executeUpdate(query);
                 System.out.println("Deleted");
             }
+            System.out.println("Connection Closed \n");
             return true;
         } catch (Exception e) {
             System.out.println("Error Deleting");
             e.printStackTrace();
             return false;
         }
+    }
+
+    public ArrayList<String[]> getAllProductos(){
+        try {
+            ArrayList<String[]> temp = new ArrayList<>();
+            Connection connection = this.createConnection();
+            Statement statement = connection.createStatement();
+            System.out.println("Searching Producto....");
+            ResultSet resultSet = statement.executeQuery("SELECT ID FROM Productos");
+            if (resultSet.next() == false) {
+                System.out.println("No results");
+                connection.close();
+                return null;
+            } else {
+                do {
+                    temp.add(this.getProducto(resultSet.getString("id")));
+                } while (resultSet.next());
+                System.out.println("Productos Found");
+                System.out.println("Connection Closed \n");
+                connection.close();
+                return temp;
+            }
+        } catch (SQLException throwables) {
+            System.out.println("Error Searching all Productos");
+            throwables.printStackTrace();
+            return null;
+        }
+    }
+
+    public String[] getUser(String adminUser, String empleadoUser){
+        try {
+            String[] temp = new String[5];
+            Connection connection = this.createConnection();
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM Users WHERE Username = ?");
+            statement.setString(1,adminUser);
+            ResultSet resultSet = statement.executeQuery();
+            if(resultSet.next()){
+                if(resultSet.getBoolean("tipo")){
+                    System.out.println("Access Granted");
+                    System.out.println("Searching User....");
+                    statement.setString(1,empleadoUser);
+                    resultSet = statement.executeQuery();
+                    if(resultSet.next()){
+                        System.out.println("User Found");
+                        temp[0] = resultSet.getString("nombre");
+                        temp[1] = resultSet.getString("documento");
+                        temp[2] = resultSet.getString("username");
+                        temp[3] = resultSet.getString("password");
+                        temp[4] = resultSet.getString("tipo");
+                    } else {
+                        System.out.println("No Results");
+                        System.out.println("Connection Closed \n");
+                        connection.close();
+                        return null;
+                    }
+                } else {
+                    System.out.println("Access Denied");
+                    System.out.println("Connection Closed \n");
+                    return null;
+                }
+            } else {
+                System.out.println("Access Denied");
+                System.out.println("Connection Closed \n");
+                return null;
+            }
+            System.out.println("Connection Closed \n");
+            return temp;
+        } catch (SQLException throwables) {
+            System.out.println("Error");
+            throwables.printStackTrace();
+            return null;
+        }
+
+    }
+
+    public boolean createUser(String adminUser, String empleadoUser, String nombre,String documento, String password, int tipo){
+        try {
+            Connection connection = this.createConnection();
+            Statement statement = connection.createStatement();
+            if (this.getUser(adminUser,adminUser) != null) {
+                String query = "INSERT INTO Users " +
+                        "VALUES ('" + nombre + "','" + documento + "','" + empleadoUser + "','" + password + "'," + tipo+")";
+                System.out.println("Inserting Row....");
+                int rows = statement.executeUpdate(query);
+                if (rows > 0)
+                    System.out.println("Row Inserted");
+                connection.close();
+                System.out.println("Connection Closed \n");
+            }
+            return true;
+        } catch (SQLException throwables) {
+            System.out.println("Error Creating User");
+            throwables.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean deleteUser(String adminUser, String empleadoUser){
+        try {
+            if(this.itsAdmin(adminUser)){
+                Connection connection = this.createConnection();
+                Statement statement = connection.createStatement();
+                String query = "select count(Username) as usercount from Users";
+                ResultSet resultSet = statement.executeQuery(query);
+                resultSet.next();
+                if(resultSet.getInt("usercount")>1){
+                    if(this.getUser(adminUser,empleadoUser) != null){
+                        query = "DELETE FROM Users " +
+                                "WHERE Username = '" + empleadoUser+"'";
+                        System.out.println("Deleting....");
+                        statement.executeUpdate(query);
+                        System.out.println("Deleted");
+                        connection.close();
+                        return true;
+                    }else{
+                        System.out.println("No results");
+                        connection.close();
+                        return false;
+                    }
+                }else{
+                    System.out.println("Cant Delete Last User");
+                    return false;
+                }
+            }
+            return false;
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean setUser(String adminUser, String empleadoUser, String nombre,String documento, String password, int tipo){
+        if(this.deleteUser(adminUser, empleadoUser)){
+            this.createUser(adminUser, empleadoUser, nombre, documento, password, tipo);
+            return true;
+        }
+        return false;
+    }
+
+    public ArrayList<String[]> getAllUsers(String adminUser){
+        try {
+            if(this.itsAdmin(adminUser)){
+                ArrayList<String[]> temp = new ArrayList<>();
+                Connection connection = this.createConnection();
+                Statement statement = connection.createStatement();
+                System.out.println("Searching Users....");
+                ResultSet resultSet = statement.executeQuery("SELECT Username FROM Users");
+                if (resultSet.next() == false) {
+                    System.out.println("No results");
+                    connection.close();
+                    return null;
+                } else {
+                    do {
+                        temp.add(this.getUser(adminUser,resultSet.getString("username")));
+                    } while (resultSet.next());
+                    System.out.println("Users Found");
+                    System.out.println("Connection Closed \n");
+                    connection.close();
+                    return temp;
+                }
+            }
+            return null;
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+            return null;
+        }
+    }
+
+    private boolean itsAdmin(String adminUser){
+        if(this.getUser(adminUser,adminUser) != null){
+            return true;
+        }
+        return false;
     }
 
     private Connection createConnection(){
